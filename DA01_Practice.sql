6e845157799b61b4efc641f55b1ff6d686966f00
slothqt <leebeeannie@gmail.com>
2024-11-08 17:44:54 +0700

    Update DA01_Practice.sql

--- DA01_Practice.sql
+++ DA01_Practice.sql
@@ -1,4 +1,26 @@
 --HW 1
+--ex1
 select name from city
 where countrycode ="USA"
-and population >120000
+and population >120000;
+--ex2
+select * from city
+where countrycode ="JPN";
+--ex3
+select city, state from station;
+--ex4
+select distinct city from station
+where city like 'a%' or city like 'e%' or city like 'i%' or city like 'o%' or city like 'u%';
+--ex5
+select distinct city from station
+where city like '%a' or city like '%e' or city like '%i' or city like '%o' or city like '%u';
+--ex6
+select distinct city from station
+where not (city like 'a%' or city like 'e%' or city like 'i%' or city like 'o%' or city like 'u%');
+--ex7
+select name from employee
+order by name;
+--ex8
+select name from employee
+where salary >2000 and months <10
+order by employee_id;
