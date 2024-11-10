fec4eeef8548beb2f9d190bdb95e7ec1e016ec30
slothqt <leebeeannie@gmail.com>
2024-11-10 15:04:36 +0700

    Update DA01_Practice 2.sql

--- DA01_Practice 2.sql
+++ DA01_Practice 2.sql
@@ -6,3 +6,8 @@ where id%2 =0;
 select count(city)-count(distinct city)
 from station;
 --ex3
+--ex4
+SELECT 
+round(sum(item_count::decimal*order_occurrences)/sum(order_occurrences),1) as mean
+FROM items_per_order;
+--ex5
