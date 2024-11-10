bb610eccea8bcc3d3973187d69776017d8a43ef9
slothqt <leebeeannie@gmail.com>
2024-11-10 18:38:30 +0700

    Update DA01_Practice 2.sql

--- DA01_Practice 2.sql
+++ DA01_Practice 2.sql
@@ -11,3 +11,10 @@ SELECT
 round(sum(item_count::decimal*order_occurrences)/sum(order_occurrences),1) as mean
 FROM items_per_order;
 --ex5
+--ex6
+SELECT user_id,
+MAX(post_date::date)-MIN(post_date::date) as days_between
+FROM posts
+where post_date between '2021-01-01' and '2022-01-01'
+group by user_id
+having count(post_id) >1;
