da369b417d4b8d7f7875f4eb55f56778a30ec55a
slothqt <leebeeannie@gmail.com>
2024-11-12 18:44:11 +0700

    Update DA01_Practice03.sql

--- DA01_Practice03.sql
+++ DA01_Practice03.sql
@@ -34,3 +34,29 @@ where sent_date >= '08-01-2022'
 group by sender_id
 order by count(message_id) desc
 limit 2;
+--ex6
+select tweet_id
+from tweets
+where length(content) >15;
+--ex7
+select activity_date as day, 
+count(distinct user_id) as active_users
+from activity
+where activity_date >='2019-06-28' and activity_date <='2019-07-27' 
+and activity_type in ('open_session','end_session','scroll_down','send_message')
+group by activity_date
+order by activity_date;
+--ex8
+select count(distinct id)
+from employees
+where extract(month from joining_date) between 1 and 7
+and extract(year from joining_date) =2022;
+--ex9
+select
+position('a' in first_name)
+from worker
+where first_name ='Amitah';
+--ex10
+select title, substring(title from length(winery)+2 for 4)
+from winemag_p2
+where country ='Macedonia';
