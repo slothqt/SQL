04c0c4609a64b85a346502904236895b2e3db41a
slothqt <leebeeannie@gmail.com>
2024-11-17 13:35:40 +0700

    Update DA01_Practice 5.sql

--- DA01_Practice 5.sql
+++ DA01_Practice 5.sql
@@ -6,3 +6,36 @@ on a.countrycode=b.code
 where b.continent is not null
 group by b.continent;
 --ex2
+SELECT
+round(sum(case when a.signup_action ='Confirmed' then 1 else 0 end)/
+count(b.user_id)::decimal,2)
+as activation_rate
+FROM emails as b
+inner join texts as a
+on a.email_id=b.email_id;
+--ex3
+SELECT a.age_bucket,
+round(sum(CASE when b.activity_type ='send' then time_spent else 0 end)*100.0/
+sum(b.time_spent),2) as send_perc,
+round(sum(CASE when b.activity_type ='open' then time_spent else 0 end)*100.0/
+sum(b.time_spent),2) as open_perc
+from age_breakdown as a
+inner join activities as b
+on a.user_id=b.user_id
+where b.activity_type in ('open','send')
+group by a.age_bucket;
+--ex4
+SELECT b.customer_id
+from products as a
+inner join customer_contracts as b
+on a.product_id=b.product_id
+group by b.customer_id
+having count(distinct a.product_category)=3;
+--ex5
+select mng.employee_id, mng.name, count(mng.employee_id) as reports_count,
+round(avg(emp.age)) as average_age
+from employees emp
+join employees mng
+on emp.reports_to=mng.employee_id
+group by mng.employee_id, mng.name
+order by mng.employee_id;
